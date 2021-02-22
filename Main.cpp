95e03be3bc4b239b7ec92d33e5c08c2dac989f94
ColinRebelo <colin.rebelo@ontariotechu.net>
2021-02-21 20:39:10 -0500

    Could not implement switch for a string so created a series of if conditions for input

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,52 @@
 #include <iostream>
+#include <string>
+#include <frontend.h>
 
 using namespace std;
 
 int main() {
-	string input;
+	Session mySes;
 
-	while true {
-		cin >> input;
+	char* in;
+
+	while (true){
+		cin >> in;
+		if(strcmp(in, "login") == 0){
+			mySes.login();
+		}
+		
+		if(strcmp(in,"logout") == 0){
+			mySes.logout();
+		}
+	
+		if(strcmp(in,"withdrawal") == 0){
+			mySes.withdrawal();
+		}
+
+		if(strcmp(in,"transfer") == 0){
+			mySes.transfer();
+		}
+		
+		if(strcmp(in,"deposit") == 0){
+			mySes.deposit();
+		}
+
+		if(strcmp(in,"changeplan") == 0){
+			mySes.changeplan();
+		}
+
+		if(strcmp(in,"delete") == 0){
+			mySes.discard();
+		}
+
+		if(strcmp(in,"disable") == 0){
+			mySes.disable();
+		}
+
+		if(strcmp(in,"create") == 0){
+			mySes.create();
+		}
+	
 	}
 
 	return 0;
