e85e989a0284e7cfe93f2967de5e2b7182759885
digital-drreams <ben.armstrong@ontariotechu.net>
2021-02-21 21:54:51 -0500

    Fixes header issues, WIP create functionality
    Namely, the #endif missing and the wrong header import notation. Whoops!

--- Session.cpp
+++ Session.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-#include <string> 
+#include <string>
+#include "frontend.h"
 
 class Session {
     private:
+        string username;
         bool isPrivileged;
         bool isActive = false;
         int transactionLogSize;
@@ -42,7 +44,30 @@ class Session {
         }
 
         void create(){
+            if (isActive) { //Is there a user logged in?
+                if (isPrivileged) { //Are they an admin?
+                    string acc;
+                    cout << "Enter account holder name: ";
+                    cin >> acc;
 
+                    if (acc.length() > 20) { //Is the name format valid?
+                        cout << "Error: Account holder name must be 20 characters or less." << endl;
+                    }
+                    else {
+                        float balance;
+                        string buffer;
+                        cout << "Enter initial balance: ";
+                        cin >> buffer;
+                        //WIP
+                    }
+                }
+                else {
+                    cout << "Permission denied." << endl;
+                }
+            }
+            else {
+                cout << "Error: Transaction not accepted outside of active session." << endl;
+            }
         }
         
 };
