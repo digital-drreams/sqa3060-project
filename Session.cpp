669bd6f14489a4f7d2d7c9be5f4092bbf98faf81
digital-drreams <ben.armstrong@ontariotechu.net>
2021-02-21 22:34:04 -0500

    Reformats Session to work properly with the header file

--- Session.cpp
+++ Session.cpp
@@ -4,115 +4,119 @@
 
 using namespace std;
 
-class Session {
-    private:
-        string username;
-        bool isPrivileged;
-        bool isActive = false;
-        int transactionLogSize;
-        std::string* transactionLog = new std::string[transactionLogSize];
+int Session::transactionLogSize;
+std::string* Account::transactionLog = new std::string[transactionLogSize];
 
-    public:
-        void login(){
-            if (!isActive){
-                string sessionType;
-                string adminType = "admin";
-                string standardType = "standard";
-                cout << "Welcome to the banking system." << endl;
-                cout << "Enter session type: ";
-                cin >> sessionType;
-                if (sessionType == adminType){
-                    cout << "Session request successful." << endl;
-                    isPrivileged = true;
-                    isActive = true;
-                } else if (sessionType == standardType){
-                    cout << "Session request successful." << endl;
-                    isPrivileged = false;
-                    isActive = true;
-                } else {
-                    cout << "Invalid session type." << endl;
-                }
-            } else {
-                cout << "Already logged in to a session" << endl;
-            }
+
+void Session::login(){
+    if (!isActive){
+        string sessionType;
+        string adminType = "admin";
+        string standardType = "standard";
+        cout << "Welcome to the banking system." << endl;
+        cout << "Enter session type: ";
+        cin >> sessionType;
+        if (sessionType == adminType){
+            cout << "Session request successful." << endl;
+            isPrivileged = true;
+            isActive = true;
+        } else if (sessionType == standardType){
+            cout << "Session request successful." << endl;
+            isPrivileged = false;
+            isActive = true;
+        } else {
+            cout << "Invalid session type." << endl;
         }
+    } else {
+        cout << "Already logged in to a session" << endl;
+    }
+}
 
-        void logout(){
-            if (isActive){
-                isActive = false;
-                cout << "Session terminated." << endl;
-                for (int i = 0; i < transactionLogSize; i++){
-                    cout << transactionLog[i];
-                }
-            } else {
-                cout << "Error: Cannot logout outside of session." << endl;
-            }
+void Session::logout(){
+    if (isActive){
+        isActive = false;
+        cout << "Session terminated." << endl;
+        for (int i = 0; i < transactionLogSize; i++){
+            cout << transactionLog[i];
         }
+    } else {
+        cout << "Error: Cannot logout outside of session." << endl;
+    }
+}
 
-        void withdrawal(){
+void Session::withdrawal(){
 
-        }
+}
 
-        void transfer(){
+void Session::transfer(){
 
-        }
+}
 
-        void deposit(){
-            if (isPrivileged){
-                string accountHolderName;
-                int accountNumber;
-                float depositValue;
+void Session::deposit(){
+    if (isPrivileged){
+        string accountHolderName;
+        int accountNumber;
+        float depositValue;
                 
-                cout << "Enter Account holder Name: ";
-                cin >> accountHolderName >> endl;
-                cout << "Enter Account Identification number: ";
-                cin >> accountNumber >> endl;
-                if (!Database.verify(accountNumber, accountHolderName)){
-                    cout << "Invalid Account Identification number." << endl;
-                }
-
-            }
+        cout << "Enter Account holder Name: ";
+        cin >> accountHolderName >> endl;
+        cout << "Enter Account Identification number: ";
+        cin >> accountNumber >> endl;
+        if (!Database.verify(accountNumber, accountHolderName)){
+            cout << "Invalid Account Identification number." << endl;
         }
 
-        void changeplan(){
+    }
+}
 
-        }
+void Session::changeplan(){
 
-        void discard(){
+}
 
-        }
+void Session::discard(){
 
-        void disable(){
+}
 
-        }
+void Session::disable(){
 
-        void create(){
-            if (isActive) { //Is there a user logged in?
-                if (isPrivileged) { //Are they an admin?
-                    string acc;
-                    cout << "Enter account holder name: ";
-                    cin >> acc;
+}
 
-                    if (acc.length() > 20) { //Is the name format valid?
-                        cout << "Error: Account holder name must be 20 characters or less." << endl;
-                    }
-                    else {
-                        float balance;
-                        string buffer;
-                        cout << "Enter initial balance: ";
-                        cin >> buffer;
-                        //WIP
-                    }
-                }
-                else {
-                    cout << "Permission denied." << endl;
-                }
+void Session::create(){
+    if (isActive) { //Is there a user logged in?
+        if (isPrivileged) { //Are they an admin?
+            string acc;
+            cout << "Enter account holder name: ";
+            cin >> acc;
+
+            if (acc.length() > 20) { //Is the name format valid?
+                cout << "Error: Account holder name must be 20 characters or less." << endl;
             }
             else {
-                cout << "Error: Transaction not accepted outside of active session." << endl;
+                float balance;
+                string buffer;
+                cout << "Enter initial balance: ";
+                cin >> buffer;
+                        
+                if (buffer.find('.') != -1) {
+                    buffer = buffer.substr(0, buffer.find('.') + 2); //Truncates long decimals
+                }
+                else {
+
+                }
+                if (buffer.length() > 6)
+
+
             }
         }
-};
+        else {
+            cout << "Permission denied." << endl;
+        }
+    }
+    else {
+        cout << "Error: Transaction not accepted outside of active session." << endl;
+    }
+}
+
 /*
 int main() {
   Session x;
