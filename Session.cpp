c1bd891d479a52829934f9ffad94c009ecdda5cf
Shayan-Bravo <shayan.khosravi@ontariotechu.net>
2021-02-21 23:46:01 -0500

    updated session

--- Session.cpp
+++ Session.cpp
@@ -66,6 +66,7 @@ bool Session::deposit(){
         if (!handler.verify(accountNumber, accountHolderName)){
             cout << "Enter amount to be deposited: ";
             cin >> depositValue;
+			handler.changeBalance(accountNumber, accountHolderName, depositValue);
         } else {
             cout << "Invalid Account Identification number." << endl;
         }
@@ -75,6 +76,7 @@ bool Session::deposit(){
         if (!handler.verify(accountNumber, username)){
             cout << "Enter amount to be deposited: ";
             cin >> depositValue;
+			handler.changeBalance(accountNumber, username, depositValue);
         } else {
             cout << "Invalid Account Identification number." << endl;
         }
