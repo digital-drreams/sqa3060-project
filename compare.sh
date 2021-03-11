4bf592deaee713c0178e10c03b2ca5a7ba040d83
digital-drreams <ben.armstrong@ontariotechu.net>
2021-03-10 21:06:24 -0500

    Comments out cmp in compare.sh, best used in emergencies if the difference isn't obvious

--- compare.sh
+++ compare.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Compares the expected and actual output folders.
 diff -rsc actual-output/ expected-output/
-cd expected-output #Navigates into the expected-output folder temporarily to grab test name prefixes
-for filename in *out.txt; do
-    cmp -c ./$filename ../actual-output/$filename;
-    cmp -c ./${filename%_*}_transact.txt ../actual-output/./${filename%_*}_transact.txt
-done
-cd .. #Returns to the directory the script was run in
+
+### optional cmp, best used if there's a stubborn difference (ie End of Line) that's not immediately obvious
+#cd expected-output #Navigates into the expected-output folder temporarily to grab test name prefixes
+#for filename in *out.txt; do
+#    cmp -c ./$filename ../actual-output/$filename;
+#    cmp -c ./${filename%_*}_transact.txt ../actual-output/./${filename%_*}_transact.txt
+#done
+#cd .. #Returns to the directory the script was run in
